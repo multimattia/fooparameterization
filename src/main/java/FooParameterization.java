5c813a8fd7bea2dc6839297fd3b7da3b2cef1806
maat <matt@mattchan.xyz>
2024-06-05 14:19:00 -0400

    Initial commit

---
+++ src/main/java/FooParameterization.java
@@ -0,0 +1,6 @@
+public class FooParameterization {
+    public static double calculateVolume(double radius) {
+        // TODO: Implement the volume calculation based on the Foo et al. parameterization
+        return 0.0;
+    }
+}
